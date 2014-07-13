131ffd41723cb52410f84b4ee995f52344cbf515
Wichert Akkerman <wichert@wiggy.net>
2014-07-13 13:36:47 +0200

    Delete unused RoundTripTestCase

--- src/pyramid_sqlalchemy/testing.py
+++ src/pyramid_sqlalchemy/testing.py
@@ -1,7 +1,4 @@
 import unittest
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.schema import MetaData
 from sqlalchemy import create_engine
 from pyramid_sqlalchemy import init_sqlalchemy
 from pyramid_sqlalchemy import metadata
@@ -31,24 +28,3 @@ class DatabaseTestCase(unittest.TestCase):
         metadata.bind = None
         self.engine.dispose()
         super(DatabaseTestCase, self).tearDown()
-
-
-class RoundTripTestCase(unittest.TestCase):
-    @classmethod
-    def setupClass(cls):
-        cls.engine = create_engine('sqlite:///', echo=True)
-        cls.metadata = MetaData(bind=cls.engine)
-        cls.BaseObject = declarative_base(metadata=cls.metadata)
-        cls.Session = sessionmaker(bind=cls.engine)
-
-    def setUp(self):
-        self.metadata.create_all()
-        self.session = self.Session()
-
-    def tearDown(self):
-        self.metadata.drop_all()
-
-    @classmethod
-    def teardownClass(cls):
-        cls.Session.close_all()
-        cls.engine.dispose()
