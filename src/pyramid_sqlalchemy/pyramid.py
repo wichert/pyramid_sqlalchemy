079ed928c967db72361e00104cb5807b7e600dda
Wichert Akkerman <wichert@wiggy.net>
2018-08-14 15:27:21 +0200

    Fix request method setup

--- src/pyramid_sqlalchemy/pyramid.py
+++ src/pyramid_sqlalchemy/pyramid.py
@@ -24,10 +24,12 @@ def includeme(config):
     """'Convenience method to initialise all components of this
     :mod:`pyramid_sqlalchemy` package from a pyramid applicaiton.
     """
-    config.add_directive('enable_sql_two_phase_commit',
-                         enable_sql_two_phase_commit)
-    config.add_request_method('sql_session', get_sql_session, reify=True)
-    config.add_request_method('sql_query', get_sql_query)
+    config.add_directive(name='enable_sql_two_phase_commit',
+                         directive=enable_sql_two_phase_commit)
+    config.add_request_method(name='sql_session',
+                              callable=get_sql_session,
+                              reify=True)
+    config.add_request_method(name='sql_query', callable=get_sql_query)
     settings = config.get_settings()
     for (name, prefix) in _find_config_prefixes(settings):
         engine = engine_from_config(settings, prefix)
